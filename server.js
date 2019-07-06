72733730af412f28672b2d76b71587b61d4c8a36
MichaelJereza <mdj1999@gmail.com>
2019-07-05 22:00:05 -0700

    Initilization and setup

--- server.js
+++ server.js
@@ -10,34 +10,64 @@ var port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
-var board = []
-for(var i=0;i<8;i++){
-    if(i%2==0){
-        board[i]={space: 'B', item: ' '}
-        board[i+8]={space: 'W', item: ' '}
-        board[i+16]={space: 'B', item: ' '}
-        board[i+24]={space: 'W', item: ' '}
-        board[i+32]={space: 'B', item: ' '}
-        board[i+40]={space: 'W', item: ' '}
-        board[i+48]={space: 'B', item: ' '}
-        board[i+56]={space: 'W', item: ' '}
-    }
-    else{
-        board[i]={space: 'W', item: ' '}
-        board[i+8]={space: 'B', item: ' '}
-        board[i+16]={space: 'W', item: ' '}
-        board[i+24]={space: 'B', item: ' '}
-        board[i+32]={space: 'W', item: ' '}
-        board[i+40]={space: 'B', item: ' '}
-        board[i+48]={space: 'W', item: ' '}
-        board[i+56]={space: 'B', item: ' '}
+//DIMENSIONS
+var max_rows = 8
+var max_cols = 8
 
+var board = []
+for(var i=0; i<max_rows; i++){
+    board[i]= new Array(8);
+}
+
+//This function initializes an array to have formatting for black and white spaces.
+function init(board,rm,cm){
+    for(var r=0;r<rm;r++){        
+        for(var c=0;c<cm;c++){
+            //if even row even col OR odd row odd col
+            if((c%2==0 && r%2==0)||(c%2!=0 && r%2!=0)){
+                board[r][c]={space: 'B', item: ' '};
+            }
+            else{
+                board[r][c]={space: 'W', item: ' '};
+            }
+        }
     }
 }
+init(board,max_rows,max_cols);
+
+function setup_board(board){
+    max=board[0].length;
+    for(var i=0;i<max;i++){
+        board[1][i].item='♙';
+        board[max-2][i].item='♟';
+        if(i==0||i==(max-1)){
+            board[0][i].item='♖';
+            board[max-1][i].item='♜';
+        }
+        else if(i==1||i==(max-2)){
+            board[0][i].item='♘';
+            board[max-1][i].item='♞';
+        }
+        else if(i==2||i==(max-3)){
+            board[0][i].item='♗';
+            board[max-1][i].item='♝';
+        }
+        else if(i==3){
+            board[0][i].item='♕';
+            board[max-1][i].item='♛';
+        }
+        else if(i==4){
+            board[0][i].item='♔';
+            board[max-1][i].item='♚';
+        }        
+    }
+}
+setup_board(board);
+
 console.log("BOARD:", board)
 app.get('/', function(req, res){
-    res.setMaxListeners(200).render('board', {
-        dimensions: board
+    res.status(200).render('board', {
+        board_rows: board
     })
     console.log("done")
 })
